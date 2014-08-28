542a62e947f563ae9cfcca629bcfd43fd519b061
basvanwesting <basvanwesting@gmail.com>
2014-08-28 22:30:42 +0200

    ToggleButton library builds test and passes Project 10b uses ToggleButton library and builds succesfully itself as well

---
+++ project_10b/project_10b.cpp
@@ -0,0 +1,40 @@
+#include "Arduino.h"
+#include "../libraries/ToggleButton/ToggleButton.h"
+
+const int controlPin1 = 2;
+const int controlPin2 = 3;
+const int enablePin = 9;
+const int potPin = A0;
+
+ToggleButton onOffButton(5);
+ToggleButton directionButton(4);
+
+int motorSpeed = 0;
+
+void setup() {
+  pinMode(controlPin1, OUTPUT);
+  pinMode(controlPin2, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin,LOW);
+}
+
+void loop() {
+  onOffButton.cycle();
+  directionButton.cycle();
+  motorSpeed = analogRead(potPin)/4;
+
+  if(directionButton.isOn()) {
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);
+  } else {
+    digitalWrite(controlPin1, LOW);
+    digitalWrite(controlPin2, HIGH);
+  }
+
+  if(onOffButton.isOn()) {
+    analogWrite(enablePin, motorSpeed);
+  } else {
+    analogWrite(enablePin, 0);
+  }
+}
+
