42d957ce4bd4a62e6fe80d1f209834e221f70797
basvanwesting <basvanwesting@gmail.com>
2014-09-10 22:03:22 +0200

    add PulseTimestampBuffer

--- libraries/Rithm2/ruby/pulse_timestamp_buffer.rb
+++ libraries/Rithm2/ruby/pulse_timestamp_buffer.rb
@@ -1,2 +1,19 @@
 class PulseTimestampBuffer
+  attr_accessor :duration, :array
+
+  def initialize(duration)
+    self.duration = duration
+    self.array = []
+  end
+
+  def <<(entry)
+    min_timestamp = entry - duration
+    array.reject! { |e| e < min_timestamp }
+    array << entry
+  end
+
+  def to_a
+    array
+  end
+
 end
--- libraries/Rithm2/ruby/spec/pulse_timestamp_buffer_spec.rb
+++ libraries/Rithm2/ruby/spec/pulse_timestamp_buffer_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe PulseTimestampBuffer do
 
-  it 'keeps timestamps of detected pulses' do
+  subject { described_class.new(4.5) }
 
-
-
-
+  it 'keeps timestamps of detected pulses within time window' do
+    subject << 5.0
+    subject.to_a.should == [5.0]
+    subject << 5.5
+    subject.to_a.should == [5.0, 5.5]
+    subject << 8.5
+    subject.to_a.should == [5.0, 5.5, 8.5]
+    subject << 9.5
+    subject.to_a.should == [5.0, 5.5, 8.5, 9.5]
+    subject << 9.6
+    subject.to_a.should == [5.5, 8.5, 9.5, 9.6]
   end
 
 end
